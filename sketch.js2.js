45cb9f7e09da7071d329a2ea2bc10535275ac4e6
deyvison124 <96534012+deyvison124@users.noreply.github.com>
2022-01-05 11:15:51 -0300

    Add files via upload

---
+++ sketch.js2.js
@@ -0,0 +1,18 @@
+
+function preload(){
+shipImg1 =loadAnimation("ship-1","ship_2","ship_3","ship_3")
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+    drawSprites();
+seaImg=loadImage("sea.png")
+
+
+
+}
